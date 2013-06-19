5ccb102dd88c6805e08ec37651dcd9e334842f65
jmiramant <josh@miramant.me>
2013-06-19 01:17:02 -0700

    resolved merge issue with data tables

--- app/models/brewery_db_worker.rb
+++ app/models/brewery_db_worker.rb
@@ -1,29 +1,30 @@
 class BreweryDbWorker
 	include Sidekiq::Worker
 
+	      fusion_access
+
+
 	def perform(location)
 		breweries_by_city = $brewery_db.locations.all(locality: location)
 		breweries_by_city.each do |breweries|
-
-      latlong = [breweries[:latitude],breweries[:longitude]].join(", ")
+      latlong = [breweries.latitude,breweries.longitude].join(", ")
 
 	    data = [{
-	      "description"          => breweries[:brewery][:description],
-	      "status_display"       => breweries[:brewery][:status_display],
-	      "website"              => breweries[:brewery][:website],
-	      "brewery_id"           => breweries[:brewery][:id],
-	      "country"              => breweries[:country][:display_name],
-	      "hours_of_operation"   => breweries[:hours_of_operation],
-	      "location_type"        => breweries[:location_type],
-	      "is_closed"            => breweries[:is_closed],
-	      "is_primary"           => breweries[:is_primary],
+	      "description"          => breweries.brewery.description,
+	      "status_display"       => breweries.brewery.status_display,
+	      "website"              => breweries.brewery.website,
+	      "brewery_id"           => breweries.brewery.id,
+	      "country"              => breweries.country.display_name,
+	      "hours_of_operation"   => breweries.hours_of_operation,
+	      "location_type"        => breweries.location_type,
+	      "is_closed"            => breweries.is_closed,
+	      "is_primary"           => breweries.is_primary,
 	      "latlong"              => latlong,
-	      "locality"     				 => breweries[:locality],
-	      "sub_name"             => breweries[:name],
-	      "location_type_display"=> breweries[:location_type],
-	      "year_opened"          => breweries[:year_opened]}]
+	      "locality"     				 => breweries.locality,
+	      "sub_name"             => breweries.name,
+	      "location_type_display"=> breweries.location_type,
+	      "year_opened"          => breweries.year_opened}]
 
-	      fusion_access
 	      location.gsub!(/\s/,'_') if location =~ /\s/ >= 0
 
 	     	tables = $ft.show_tables
